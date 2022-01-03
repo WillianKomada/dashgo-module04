c5ef69ef88de429d65c091b378b4e9f5faeebfd4
Willian Komada <willian10komada@hotmail.com>
2022-01-03 09:08:11 -0300

    componentizando os componentes

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -20,7 +20,7 @@ export default function CreateUser() {
       <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
         <Sidebar />
 
-        <Box flex="1" bordrRadius={8} bg="gray.800" p="8">
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
           <Heading size="lg" fontWeight="normal">
             Criar usuário
           </Heading>
